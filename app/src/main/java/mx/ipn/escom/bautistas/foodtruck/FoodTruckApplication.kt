cb6c279cc3defd50a9e6e3b29849804f5e0b6749
Samuel Bautista <samuelbaubas@gmail.com>
2024-01-11 10:43:35 -0600

    Application para dependecias usando dagger hilt

---
+++ app/src/main/java/mx/ipn/escom/bautistas/foodtruck/FoodTruckApplication.kt
@@ -0,0 +1,8 @@
+package mx.ipn.escom.bautistas.foodtruck
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FoodTruckApplication: Application() {
+}
