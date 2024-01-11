6eaa46a4e1bb90656d821e76bb00d5675eefeb37
Samuel Bautista <samuelbaubas@gmail.com>
2024-01-11 10:52:37 -0600

    Agregar la logica de nuestro inicio de sesion

---
+++ app/src/main/java/mx/ipn/escom/bautistas/foodtruck/ui/main/viewmodels/AuthViewModel.kt
@@ -0,0 +1,33 @@
+package mx.ipn.escom.bautistas.foodtruck.ui.main.viewmodels
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import mx.ipn.escom.bautistas.foodtruck.data.auth.model.SignInResult
+import mx.ipn.escom.bautistas.foodtruck.ui.main.interaction.AuthState
+import javax.inject.Inject
+
+@HiltViewModel
+class AuthViewModel
+    @Inject constructor(): ViewModel() {
+
+    private val _authState = MutableStateFlow(AuthState())
+    val authState = _authState.asStateFlow()
+
+    fun onSignInResult(result: SignInResult) {
+        _authState.update {
+            it.copy(
+                isSignInSuccessful = result.data != null,
+                errorMessage = result.errorMessage
+            )
+        }
+    }
+
+    fun resetState() {
+        _authState.update {
+            AuthState()
+        }
+    }
+}
