899a94bbe32cb35f34c8ffd3f15e0d641c2148ad
Samuel Bautista <samuelbaubas@gmail.com>
2024-01-11 20:51:31 -0600

    Agregar SignInScreen con toda la logica y su viewmodel

--- app/src/main/java/mx/ipn/escom/bautistas/foodtruck/ui/main/views/SignInScreen.kt
+++ app/src/main/java/mx/ipn/escom/bautistas/foodtruck/ui/main/views/SignInScreen.kt
@@ -1,43 +1,36 @@
 package mx.ipn.escom.bautistas.foodtruck.ui.main.views
 
 import android.widget.Toast
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.google.android.gms.auth.api.signin.GoogleSignIn
 import mx.ipn.escom.bautistas.foodtruck.R
 import mx.ipn.escom.bautistas.foodtruck.ui.components.ButtonComponent
 import mx.ipn.escom.bautistas.foodtruck.ui.components.CircularButtonComponent
 import mx.ipn.escom.bautistas.foodtruck.ui.components.TextFieldComponent
+import mx.ipn.escom.bautistas.foodtruck.ui.components.TextUnderlinedButton
 import mx.ipn.escom.bautistas.foodtruck.ui.main.interaction.AuthState
 
 @Composable
 fun SignInScreen(
     modifier: Modifier = Modifier,
     state: AuthState,
-    onGoogleSignIn: () -> Unit
+    onSignIn: () -> Unit,
+    navigateToSignUp: () -> Unit,
+    onGoogleSignIn: () -> Unit,
 ) {
     val context = LocalContext.current
 
@@ -51,7 +44,12 @@ fun SignInScreen(
         }
     }
 
-    Scaffold {
+    Scaffold(bottomBar = {
+        TextUnderlinedButton(label = stringResource(id = R.string.sign_up_main_text)) {
+            navigateToSignUp()
+        }
+
+    }) {
         Box(modifier = modifier.padding(it)) {
             Column(
                 modifier = modifier.padding(50.dp),
@@ -62,7 +60,7 @@ fun SignInScreen(
                         .height(240.dp)
                         .fillMaxWidth(), contentAlignment = Alignment.Center
                 ) {
-                    Text(text = stringResource(R.string.sing_in_main_text))
+                    Text(text = stringResource(R.string.sign_in_main_text))
                 }
                 TextFieldComponent(
                     label = stringResource(id = R.string.email_label),
@@ -72,7 +70,7 @@ fun SignInScreen(
                     onChanged = {})
                 Spacer(modifier = modifier.height(10.dp))
                 ButtonComponent(label = stringResource(id = R.string.button_sign_in_label)) {
-
+                    onSignIn()
                 }
                 Text(text = stringResource(id = R.string.sign_in_with_text))
                 CircularButtonComponent {
@@ -84,8 +82,8 @@ fun SignInScreen(
     }
 }
 
-//@Preview
-//@Composable
-//fun SignInScreenPrev() {
-//    SignInScreen(){}
-//}
+@Preview
+@Composable
+fun SignInScreenPrev() {
+    SignInScreen(state = AuthState(), onSignIn = {}, navigateToSignUp = {}) {}
+}
