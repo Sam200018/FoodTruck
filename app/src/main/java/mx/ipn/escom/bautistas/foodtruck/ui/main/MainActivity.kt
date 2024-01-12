61823aaef08147d803e3aa454b1eaaa60f60a1a3
Samuel Bautista <samuelbaubas@gmail.com>
2024-01-11 20:53:02 -0600

    Cambiar signup y homescreen agregando su toast y su reacciones al state

--- app/src/main/java/mx/ipn/escom/bautistas/foodtruck/ui/main/MainActivity.kt
+++ app/src/main/java/mx/ipn/escom/bautistas/foodtruck/ui/main/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -19,10 +20,13 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import mx.ipn.escom.bautistas.foodtruck.R
 import mx.ipn.escom.bautistas.foodtruck.data.auth.model.GoogleAuthUIClient
+import mx.ipn.escom.bautistas.foodtruck.data.auth.model.SignInResult
 import mx.ipn.escom.bautistas.foodtruck.ui.main.viewmodels.AuthViewModel
+import mx.ipn.escom.bautistas.foodtruck.ui.main.viewmodels.SignUpViewModel
 import mx.ipn.escom.bautistas.foodtruck.ui.main.views.HomeScreen
 import mx.ipn.escom.bautistas.foodtruck.ui.main.views.Routes
 import mx.ipn.escom.bautistas.foodtruck.ui.main.views.SignInScreen
+import mx.ipn.escom.bautistas.foodtruck.ui.main.views.SignUpScreen
 import mx.ipn.escom.bautistas.foodtruck.ui.theme.FoodTruckTheme
 import javax.inject.Inject
 
@@ -32,6 +36,7 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var googleAuthUIClient: GoogleAuthUIClient
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -51,6 +56,18 @@ class MainActivity : ComponentActivity() {
                     startDestination = startDestination
                 ) {
                     composable(Routes.SignInScreen.route) {
+                        LaunchedEffect(key1 = Unit) {
+                            if (googleAuthUIClient.getSignedInUser() != null) {
+                                authViewModel.onSignInResult(
+                                    SignInResult(
+                                        googleAuthUIClient.getSignedInUser(),
+                                        errorMessage = ""
+                                    )
+                                )
+                            }
+                        }
+
+
                         val launcher = rememberLauncherForActivityResult(
                             contract = ActivityResultContracts.StartIntentSenderForResult(),
                             onResult = { result ->
@@ -73,11 +90,83 @@ class MainActivity : ComponentActivity() {
                                     R.string.login_successful_text,
                                     Toast.LENGTH_LONG,
                                 ).show()
-//                                authViewModel.resetState()
                             }
                         }
 
-                        SignInScreen(state = state) {
+                        SignInScreen(state = state, onSignIn = {}, onGoogleSignIn = {
+                            lifecycleScope.launch {
+                                val signinIntentSender = googleAuthUIClient.signIn()
+                                launcher.launch(
+                                    IntentSenderRequest.Builder(
+                                        signinIntentSender ?: return@launch
+                                    ).build()
+                                )
+                            }
+                        }, navigateToSignUp = {
+                            navController.navigate(Routes.SignupScreen.route)
+                        })
+
+                    }
+
+                    composable(Routes.HomeScreen.route) {
+                        HomeScreen() {
+                            lifecycleScope.launch {
+                                googleAuthUIClient.signOut()
+                                Toast.makeText(
+                                    applicationContext,
+                                    R.string.logout_text,
+                                    Toast.LENGTH_LONG
+                                )
+                                authViewModel.resetState()
+                            }
+                        }
+                    }
+
+                    composable(Routes.SignupScreen.route) {
+
+
+                        val launcher = rememberLauncherForActivityResult(
+                            contract = ActivityResultContracts.StartIntentSenderForResult(),
+                            onResult = { result ->
+                                if (result.resultCode == RESULT_OK) {
+                                    lifecycleScope.launch {
+                                        val signInResult =
+                                            googleAuthUIClient.googleSignInWithIntent(
+                                                intent = result.data ?: return@launch
+                                            )
+                                        authViewModel.onSignInResult(signInResult)
+                                    }
+                                }
+                            }
+                        )
+
+                        LaunchedEffect(key1 = state.isSignInSuccessful) {
+                            if (state.isSignInSuccessful) {
+                                Toast.makeText(
+                                    applicationContext,
+                                    R.string.login_successful_text,
+                                    Toast.LENGTH_LONG,
+                                ).show()
+                            }
+                        }
+
+                        val signUpViewModel: SignUpViewModel by viewModels()
+
+
+                        SignUpScreen(
+                            signUpViewModel = signUpViewModel,
+                            onSignUp = {
+                                signUpViewModel.createAccount {
+                                    navController.popBackStack()
+                                    signUpViewModel.clearFields()
+                                }
+                                signUpViewModel.resetState()
+                            },
+                            back = {
+                                navController.popBackStack()
+                                signUpViewModel.clearFields()
+                                signUpViewModel.resetState()
+                            }) {
                             lifecycleScope.launch {
                                 val signinIntentSender = googleAuthUIClient.signIn()
                                 launcher.launch(
@@ -87,10 +176,6 @@ class MainActivity : ComponentActivity() {
                                 )
                             }
                         }
-
-                    }
-                    composable(Routes.HomeScreen.route){
-                        HomeScreen()
                     }
                 }
 
