dc5ac8715c688a3ba37001fd608732ef6dfdf0c9
Samuel Bautista <samuelbaubas@gmail.com>
2024-01-11 20:45:02 -0600

    Agregar firestore para guardar a los usuarios y su info relevante

--- app/src/main/java/mx/ipn/escom/bautistas/foodtruck/data/auth/model/GoogleAuthUIClient.kt
+++ app/src/main/java/mx/ipn/escom/bautistas/foodtruck/data/auth/model/GoogleAuthUIClient.kt
@@ -3,19 +3,22 @@ package mx.ipn.escom.bautistas.foodtruck.data.auth.model
 import android.content.Context
 import android.content.Intent
 import android.content.IntentSender
+import android.util.Log
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 import mx.ipn.escom.bautistas.foodtruck.R
 import kotlin.coroutines.cancellation.CancellationException
 
-class GoogleAuthUIClient (
+class GoogleAuthUIClient(
     private val context: Context,
     private val oneTapClient: SignInClient,
-    private val auth: FirebaseAuth
-){
+    private val auth: FirebaseAuth,
+    private val db: FirebaseFirestore,
+) {
     suspend fun signIn(): IntentSender? {
         val result = try {
             oneTapClient.beginSignIn(
@@ -34,15 +37,26 @@ class GoogleAuthUIClient (
         val googleIdToken = credential.googleIdToken
 
         val googleCredentials = GoogleAuthProvider.getCredential(googleIdToken, null)
+
         return try {
             val user = auth.signInWithCredential(googleCredentials).await().user
+
             SignInResult(
                 data = user?.run {
-                    UserData(
+                    val userData = UserData(
                         userId = uid,
                         userName = displayName,
-                        profilePictureUri = photoUrl?.toString()
+                        profilePictureUri = photoUrl?.toString(),
+                        email = email,
+                        userType = null
                     )
+                    val infoMap = hashMapOf(
+                        "info" to userData.toMap()
+                    )
+                    db.collection("users").document(uid)
+                        .set(infoMap)
+
+                    userData
                 },
                 errorMessage = null
             )
@@ -67,12 +81,36 @@ class GoogleAuthUIClient (
         }
     }
 
-    fun getSignedInUser(): UserData? = auth.currentUser?.run {
-        UserData(
-            userId = uid,
-            userName = displayName,
-            profilePictureUri = photoUrl?.toString()
-        )
+    suspend fun getSignedInUser(): UserData? {
+
+
+        return auth.currentUser?.run {
+            val userDoc = db.collection("users").document(uid).get().await()
+            if (userDoc.exists()) {
+                val infoMap = userDoc["info"] as? HashMap<String, String?> ?: hashMapOf()
+                Log.i(
+                    "infoMap",
+                    infoMap.toString()
+                )
+
+                UserData(
+                    userId = uid,
+                    userName = displayName,
+                    profilePictureUri = infoMap["profilePictureUri"],
+                    email = email,
+                    userType = infoMap["user_type"]
+                )
+            } else {
+                UserData(
+                    userId = uid,
+                    userName = displayName,
+                    profilePictureUri = photoUrl?.toString(),
+                    email = email,
+                    userType = null
+                )
+            }
+
+        }
     }
 
     private fun buildSignInRequest(): BeginSignInRequest {
