5b24501768f56a0814d8a6a340d7bb56ed5923fa
Samuel Bautista <samuelbaubas@gmail.com>
2024-01-11 20:50:54 -0600

    Cambiar home screen agregando boton de signout

--- app/src/main/java/mx/ipn/escom/bautistas/foodtruck/ui/main/views/HomeScreen.kt
+++ app/src/main/java/mx/ipn/escom/bautistas/foodtruck/ui/main/views/HomeScreen.kt
@@ -2,13 +2,16 @@ package mx.ipn.escom.bautistas.foodtruck.ui.main.views
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,11 +19,21 @@ import mx.ipn.escom.bautistas.foodtruck.ui.components.TextFieldComponent
 
 @Composable
 fun HomeScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    signOut: () -> Unit
 ) {
     Scaffold {
-        Box(modifier.padding(it)) {
-            Text(text = "Hola")
+        Box(
+            modifier
+                .padding(it)
+                .fillMaxSize(), contentAlignment = Alignment.Center
+        ) {
+
+            Button(onClick = {
+                signOut()
+            }) {
+                Text(text = "Cerrar sesion")
+            }
         }
     }
 }
@@ -28,5 +41,5 @@ fun HomeScreen(
 @Preview(showSystemUi = true)
 @Composable
 fun HomeScreenPrev() {
-    HomeScreen()
+    HomeScreen() {}
 }
