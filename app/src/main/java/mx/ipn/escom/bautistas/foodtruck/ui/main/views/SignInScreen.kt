9b01cdf67865d25a08c4214b10db94ec0258f18c
Samuel Bautista <samuelbaubas@gmail.com>
2024-01-12 19:04:50 -0600

    Agregar sign in state como interaccion en sign in screen

--- app/src/main/java/mx/ipn/escom/bautistas/foodtruck/ui/main/views/SignInScreen.kt
+++ app/src/main/java/mx/ipn/escom/bautistas/foodtruck/ui/main/views/SignInScreen.kt
@@ -1,5 +1,6 @@
 package mx.ipn.escom.bautistas.foodtruck.ui.main.views
 
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,31 +12,51 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import mx.ipn.escom.bautistas.foodtruck.R
 import mx.ipn.escom.bautistas.foodtruck.ui.components.ButtonComponent
 import mx.ipn.escom.bautistas.foodtruck.ui.components.CircularButtonComponent
 import mx.ipn.escom.bautistas.foodtruck.ui.components.TextFieldComponent
 import mx.ipn.escom.bautistas.foodtruck.ui.components.TextUnderlinedButton
 import mx.ipn.escom.bautistas.foodtruck.ui.main.interaction.AuthState
+import mx.ipn.escom.bautistas.foodtruck.ui.main.interaction.formValid
+import mx.ipn.escom.bautistas.foodtruck.ui.main.viewmodels.SignInViewModel
 
 @Composable
 fun SignInScreen(
     modifier: Modifier = Modifier,
-    state: AuthState,
+    authState: AuthState,
     onSignIn: () -> Unit,
     navigateToSignUp: () -> Unit,
     onGoogleSignIn: () -> Unit,
+    signInViewModel: SignInViewModel,
 ) {
+
+
     val context = LocalContext.current
 
-    LaunchedEffect(key1 = state) {
-        state.errorMessage?.let { error ->
+    LaunchedEffect(key1 = authState) {
+        if (authState.errorMessage != null) {
+
+            authState.errorMessage.let { error ->
+                Toast.makeText(
+                    context,
+                    error,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    val signInState by signInViewModel.signInState.collectAsStateWithLifecycle()
+    LaunchedEffect(key1 = signInState) {
+        signInState.errorMessage?.let { error ->
             Toast.makeText(
                 context,
                 error,
@@ -63,13 +84,29 @@ fun SignInScreen(
                     Text(text = stringResource(R.string.sign_in_main_text))
                 }
                 TextFieldComponent(
+                    value = signInViewModel.emailValue,
+                    isError = signInState.isEmailValid.not(),
+                    errorMessage = stringResource(id = R.string.email_not_valid),
                     label = stringResource(id = R.string.email_label),
-                    onChanged = {})
+                    onChanged = {
+                        signInViewModel.onEmailChange(it)
+                    })
                 TextFieldComponent(
+                    value = signInViewModel.passwordValue,
+                    isError = signInState.isPasswordValid.not(),
+                    errorMessage = stringResource(id = R.string.password_not_valid),
                     label = stringResource(id = R.string.password_label),
-                    onChanged = {})
+                    onChanged = {
+                        signInViewModel.onPasswordChange(it)
+                    })
                 Spacer(modifier = modifier.height(10.dp))
-                ButtonComponent(label = stringResource(id = R.string.button_sign_in_label)) {
+                ButtonComponent(
+                    label = if (signInState.isLoading.not())
+                        stringResource(id = R.string.button_sign_in_label)
+                    else
+                        stringResource(id = R.string.loading_text),
+                    isEnable = signInState.formValid() or signInState.isLoading
+                ) {
                     onSignIn()
                 }
                 Text(text = stringResource(id = R.string.sign_in_with_text))
@@ -82,8 +119,3 @@ fun SignInScreen(
     }
 }
 
-@Preview
-@Composable
-fun SignInScreenPrev() {
-    SignInScreen(state = AuthState(), onSignIn = {}, navigateToSignUp = {}) {}
-}
