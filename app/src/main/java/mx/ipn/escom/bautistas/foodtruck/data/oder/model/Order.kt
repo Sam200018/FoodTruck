46f1a0976646a38cb429cf37c8df903c87bc9689
Samuel Bautista <samuelbaubas@gmail.com>
2024-01-16 20:38:00 -0600

    Actualizacion de order model

--- app/src/main/java/mx/ipn/escom/bautistas/foodtruck/data/oder/model/Order.kt
+++ app/src/main/java/mx/ipn/escom/bautistas/foodtruck/data/oder/model/Order.kt
@@ -1,6 +1,7 @@
 package mx.ipn.escom.bautistas.foodtruck.data.oder.model
 
 data class Order(
+    val id: String? = null,
     val product: Int = 0,
     val amount: Int = 0,
     val lat: Double? = 0.0,
@@ -19,9 +20,13 @@ fun Order.toMap(): HashMap<String, Any?> {
         "long" to long,
         "building" to building,
         "comment" to comment,
-        "userOder" to userOrder,
+        "userOrder" to userOrder,
         "userWorker" to userWorker
     )
 }
+
+//fun Order.fromMap(id: String?, data: Map<String, Object> ): Order{
+//    return  Order(id,product= data["product"],)
+//}
 
 
